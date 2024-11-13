9f1572542645aaeb85d8614e1a384ec3c3d418a2
DIEME <37548785+diemenaby@users.noreply.github.com>
2024-11-13 13:27:19 +0000

    Add files via upload

---
+++ error.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 13 13:24:03 2024
+
+@author: USER
+"""
+
+mylist=[14, "hello", 967]
+
+mylist[2]
+
+pip install pandas
+
+import Pandas
+import NumPy
+
+print("python errors")
+
+
+mydictionnary={True:"hello",False:"bye", '3':"python"}
+mydictionnary[True]
+
+i=14
+while i<78:
+    print(i)
+    i+=1
+
+
+
+
+it=iter([1,2,3])
+next(it)
+next(it)
+next(it)
+
+15+15
+
+
+str('python')
+
+
+python = 'python'
+
+
+x=19/1
